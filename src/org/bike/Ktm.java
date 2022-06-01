1eae66c98d6f7ba4afb4613ee1f19635a87f0eda
Mala <mala.krishnaraj.slm@gmail.com>
2022-06-01 22:31:02 +0530

    Welcome

---
+++ src/org/bike/Ktm.java
@@ -0,0 +1,32 @@
+package org.bike;
+
+public class Ktm implements Bike
+
+{
+	public void cost() {
+		System.out.println("Cost Of the bike:60,000");
+	}
+
+	public void speed() {
+		System.out.println("Speed of the bike: 200cc");
+
+	}
+
+	public static void main(String[] args) {
+	Ktm k =new Ktm();
+	k.cost();
+	k.speed();
+	
+	
+	
+	
+	
+	}
+
+	
+	
+	
+	
+	
+  
+}
